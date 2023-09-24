703dd985af90bda8fe1272e53cdc1a2fd06317a1
Arjun Verma <arjun.verma8412@gmail.com>
2023-09-24 10:46:42 +0530

    init

---
+++ lib/simple_proxy.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "simple_proxy/version"
+
+module SimpleProxy
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/simple_proxy/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SimpleProxy
+  VERSION = "0.1.0"
+end
